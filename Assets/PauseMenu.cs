8b85c03bbb4f9d19a6880c23248c667f26f2c26d
Payton <89274535+connepay000@users.noreply.github.com>
2023-12-17 15:37:22 -0500

    Glitches Fixed

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,15 +7,15 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool IsGamePaused = false;
-
+    public PlayerHealth p;
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && p.getHealth() > 0)
         {
-            if (IsGamePaused)
+            if (pauseMenuUI.activeSelf)
             {
                 Resume();
             }
@@ -30,14 +30,12 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        IsGamePaused = false;
     }
 
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        IsGamePaused = true;
     }
 
     public void LoadMenu()
