41284910eae1ea09b4f9823166197a0edf8f91fa
Payton <89274535+connepay000@users.noreply.github.com>
2023-12-17 11:44:47 -0500

    Player Coins updates

--- Assets/Scripts/PlayerScripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerScripts/PlayerInventory.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class PlayerInventory : MonoBehaviour
 {
     public PlayerInventoryManagment i;
+    public TextMeshProUGUI numberText;
+
+    public void Update()
+    {
+        setNumberText();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Coin")) return;
-        Debug.Log("Cha Ching!");
-        Debug.Log("Got a " + other.GetComponent<CoinInfo>().CoinType);
         i.setWorth(other.GetComponent<CoinInfo>().Amount + i.getWorth()) ;
-        Debug.Log("You are now worth " + i.getWorth() + " gold coins.") ;
         Destroy(other.gameObject);
+        setNumberText();
+    }
+
+    public void setNumberText()
+    {
+        numberText.text = i.getWorth().ToString();
     }
 
 }
