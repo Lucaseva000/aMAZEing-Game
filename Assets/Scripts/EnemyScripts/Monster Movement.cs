f5a20c2e10dbf014d3d49cf9f28e32cdaf006ae9
Payton <89274535+connepay000@users.noreply.github.com>
2024-01-17 16:46:07 -0500

    Everything works

--- Assets/Scripts/EnemyScripts/Monster Movement.cs
+++ Assets/Scripts/EnemyScripts/Monster Movement.cs
@@ -13,33 +13,23 @@ public class MonsterMovement : MonoBehaviour
     public Rigidbody2D enemyRB;
     private bool isChasing;
     public float chaseDistance;
-<<<<<<< HEAD
-<<<<<<< HEAD
     public float chaseLose;
-    
-=======
-=======
     public float totalChaseTime = 5;
     private float currentChaseTime;
->>>>>>> 6cc97892325b65bfb0937aee1b79fc7ecd30c275
-
     [Header("KnockBack")]
     [SerializeField] public float KBForce;
     [SerializeField] public float KBCounter;
     public float KBTotalTime;
     public bool KnockFromRight;
 
-<<<<<<< HEAD
->>>>>>> 222310568f4f8a5164a5be8b1f6d61a456ba86fa
-=======
->>>>>>> 6cc97892325b65bfb0937aee1b79fc7ecd30c275
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
         playerTransform = GameObject.FindWithTag("Player").transform;
         if(KBCounter <= 0)
-<<<<<<< HEAD
+
         {
             movement();
         }
@@ -61,37 +51,13 @@ public class MonsterMovement : MonoBehaviour
         
     }
 
-    public void movement()
-    {
-        if (isChasing)
-        {
-=======
-        {
-            movement();
-        }
-        else
-        {
-            if(KBCounter == KBTotalTime)
-            {
-                if (KnockFromRight)
-                {
-                    enemyRB.velocity = new Vector2(-KBForce, (KBForce/2));
-                }
-                else if (!KnockFromRight)
-                {
-                    enemyRB.velocity = new Vector2(KBForce, (KBForce/2));
-                }
-            }
-            KBCounter -= Time.deltaTime;
-        }
-        
-    }
+ 
 
     public void movement()
     {
         if (isChasing && currentChaseTime > 0)
         {
->>>>>>> 6cc97892325b65bfb0937aee1b79fc7ecd30c275
+
             if (transform.position.x > playerTransform.position.x)
             {
                 transform.localScale = new Vector3(2, 2, 1);
@@ -108,12 +74,9 @@ public class MonsterMovement : MonoBehaviour
         }
         else
         {
-<<<<<<< HEAD
-=======
             isChasing = false;
             currentChaseTime = totalChaseTime;
 
->>>>>>> 6cc97892325b65bfb0937aee1b79fc7ecd30c275
             if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
             {
                 isChasing = true;
